fd93d892cd4a851680e4ccc9159e726e2a897e35
andersonlemos <anderson.lemos@dmorj.com.br>
2017-12-28 10:22:24 -0200

    merge commit

---
+++ js/principal.js
@@ -0,0 +1,10 @@
+var titulo = document.querySelector(".titulo")
+titulo.textContent =  "Aparecida Nutricionista";
+
+var trPaciente = document.querySelector("#primeiro-paciente")
+var tdPeso = trPaciente.querySelector(".info-peso").textContent
+var tdAltura =  trPaciente.querySelector(".info-altura").textContent
+
+console.log(trPaciente);
+console.log(tdPeso);
+console.log(tdAltura);
